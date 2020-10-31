ecfdc5149079ad09a7dc33a5c22d65eca431c118
Khaildoran <sgermscheid@gmail.com>
2020-10-31 06:40:31 -0500

    installed and imported redux/react-redux  and react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+
 import './App.css';
+import Header from '../Header/Header'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+    return ( 
+      <Router>
+        <Header/>
+          
+        <Route exact path="/" />
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+// dependecies 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import logger from 'redux-logger';
+//Component
+import App from './components/App/App';
+//Misc
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const reduxStore = createStore(
+    combineReducers({
+      
+    }),
+    applyMiddleware(logger)
+  );
+  
+
+ReactDOM.render(
+    <Provider store={reduxStore}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
